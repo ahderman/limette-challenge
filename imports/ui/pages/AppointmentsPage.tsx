7c82f200c655aeb123bb346b4ee8b3df3c0d0680
Alexandre D'Erman <alex@derman.it>
2024-02-17 19:10:35 +0100

    Add user name to the as-yet-unstyled header

--- imports/ui/pages/AppointmentsPage.tsx
+++ imports/ui/pages/AppointmentsPage.tsx
@@ -1,15 +1,27 @@
 import React, { useState } from 'react';
+// @ts-ignore
+import { useTracker } from 'meteor/react-meteor-data';
+
 import { LogoutButton } from '/imports/ui/components/LogoutButton';
 import { AppointmentList } from '/imports/ui/components/AppointmentList';
 import { AppointmentFilter } from '/imports/ui/components/AppointmentFilter';
+import * as auth from '/imports/api/auth';
 
 export const AppointmentsPage = () => {
   const [filterText, setFilterText] = useState<string>('');
 
+  const currentUser = useTracker(auth.getCurrentUser);
+
   return (
     <div>
-      <h1>LiMetTe</h1>
-      <LogoutButton />
+      <header>
+        <h1>LiMetTe</h1>
+        <p>
+          ☻ Logged in as <em>{currentUser?.username}</em>
+        </p>
+        <LogoutButton />
+      </header>
+
       <AppointmentFilter onFilterTextChange={setFilterText} />
       <AppointmentList filterText={filterText} />
     </div>
