fc371aeaec798ea4f4eccf7d997e3ba9adc014d2
Alexandre D'Erman <alex@derman.it>
2024-02-18 23:27:23 +0100

    Add header to the login screen

--- imports/ui/pages/LoginPage.tsx
+++ imports/ui/pages/LoginPage.tsx
@@ -5,6 +5,12 @@ import { LoginForm } from '/imports/ui/components/LoginForm';
 export const LoginPage = () => {
   return (
     <div className="login-page">
+      <header>
+        <div className="header-logo-wrapper">
+          <h1>LiMetTe</h1>
+        </div>
+      </header>
+
       <LoginForm />
     </div>
   );
